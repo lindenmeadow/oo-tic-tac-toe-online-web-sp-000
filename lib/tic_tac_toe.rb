409a9a190e78856b63840b199b4b8d8279e1522d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-26 01:02:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,10 +34,8 @@ class TicTacToe
     ((@board[index_i] == "X") || (@board[index_i] == "O"))
   end
 
-  def valid_move?(board, index)
-    @board = board
-    @index = index
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn_count
